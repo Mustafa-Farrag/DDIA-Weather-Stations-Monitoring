81798eb4ce418a0b1048c657981b70c9c87b47ab
Mustafa_Farrag <76589515+Mustafa-Farrag@users.noreply.github.com>
2024-05-11 01:41:02 +0300

    Delete Rain_Detector directory

--- Rain_Detector/src/main/java/org/example/Main.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-
-public class Main {
-    public static void main(String[] args) {
-        Thread rainDetectorThread = new Thread(RainDetectorProcessor::detectRain);
-        rainDetectorThread.start();
-    }
-}
